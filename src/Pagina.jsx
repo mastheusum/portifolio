baefdb0ed970c546d3020d10b2cd3ba9b618ea3e
victor maceio <victor.maceio@supergeeks.com.br>
2024-02-20 16:59:12 -0300

    Fazendo Deploy

---
+++ src/Pagina.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+export default function Pagina() {
+  return (
+    <main>
+      <img src="./picture.jpg" alt="foto" width="128" />
+      <div> 
+          <b> Victor Matheus </b> | 00 anos | Desenvolvedor Web
+      </div> 
+      <div>
+          <b> Perfil </b> : Olá eu sou o Victor Matheus e sou Desenvolvedor Web.
+          Adoro programar em React e em outras diversas tecnologias novas.
+      </div>
+      <div>
+        <b> Experiências </b> : Já trabalhei com Ruby on Rails, PHP, React,
+        desenvolvendo projetos como Netflix
+        e passei 00 anos fazendo curso na SuperGeeks.
+      </div>
+      <div>
+        <b> Competências </b> :
+        Lógica de programação,
+        Raciocínio computacional,
+        Python,
+        C#,
+        Ruby,
+        PHP,
+        HTML,
+        CSS,
+        JavaScript e
+        React. 
+      </div>
+      <div>
+        <a href="mailto:victor.matheus.xd@gmail.com"> Email </a> 
+        <a href="tel:+55(82)99959-8166"> Celular </a>
+        <a href="https://github.com/mastheusum"> Github </a> 
+      </div>
+    </main>
+  )
+}
